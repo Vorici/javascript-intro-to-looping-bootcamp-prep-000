5731329d2c5c352ddf4b68ea3562df6e7c2423d1
Ledio Bode <l.bode515@gmail.com>
2018-04-12 07:02:10 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,3 +13,17 @@ function forLoop(array) {
   return array;
   
 }
+
+function whileLoop(n) {
+
+  let countdown = n;
+
+    while (countdown > 0) {
+      console.log(--countdown)
+	  
+    }
+	
+    return "done";
+	
+  }
+
